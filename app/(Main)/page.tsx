adae2d9d5413d3ad2b760597d061e2c8f79c8633
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-02-09 15:00:06 -0800

    About component Added

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -2,6 +2,8 @@
 import Header from "@/components/Header";
 import React, { useEffect, useState } from "react";
 import Hero from "@/components/Route/Hero";
+import Image from "next/image";
+import About from "@/components/Route/About";
 
 type Props = {};
 
@@ -22,6 +24,19 @@ const Page = (props: Props) => {
       <div className="banner">
         <Header activeItem={0} />
         <Hero />
+        <div>
+          <Image
+            src={"https://pixner.net/aikeu/assets/images/footer/shape-two.png"}
+            width={120}
+            height={120}
+            alt=""
+            className="absolute right-[30px]"
+          />
+          <br />
+          <div className="w-[95%] md:w-[90%] xl:w-[80%] 2xl:w-[75%] m-auto">
+            <About/>
+          </div>
+        </div>
       </div>
     </div>
   );
