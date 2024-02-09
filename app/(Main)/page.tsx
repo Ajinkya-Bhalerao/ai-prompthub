4b7c225b11ec717b85edfae91c68a379528b4c13
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-02-09 14:08:46 -0800

    Banner Component Completed

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,15 +1,30 @@
-'use client'
-import Header from '@/components/Header';
-import React from 'react'
+"use client";
+import Header from "@/components/Header";
+import React, { useEffect, useState } from "react";
+import Hero from "@/components/Route/Hero";
 
-type Props = {}
+type Props = {};
 
 const Page = (props: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    if (!isMounted) {
+      setIsMounted(true);
+    }
+  }, [isMounted]);
+
+  if (!isMounted) {
+    return null;
+  }
   return (
     <div>
-      <Header activeItem={0} />
+      <div className="banner">
+        <Header activeItem={0} />
+        <Hero />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
